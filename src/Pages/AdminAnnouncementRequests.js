5f922cea0e87a304988aec8eaf262d79338d8bef
berk-sengul <berk.sengul@davon.com.tr>
2024-05-07 12:17:31 +0300

    create announcement request component, add backend integration

--- src/Pages/AdminAnnouncementRequests.js
+++ src/Pages/AdminAnnouncementRequests.js
@@ -1,28 +1,19 @@
-import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "../CSS/AdminAnnouncementRequests.css";
+import AnnouncementRequest from "../components/AnnouncementRequest";
 import Header from "../components/Header";
 import SystemAdminSidebar from "../components/SystemAdminSidebar";
 
 function AdminAnnouncementRequests() {
-  const [announcements, setAnnouncements] = useState([]);
-
-  const handleActionClick = (action, id) => {
-    alert(`${action} clicked for announcement ${id}`);
-  };
-
-  const handleContentClick = () => {
-    // Placeholder for opening PDF
-    alert("Content Clicked!");
-  };
+  const [announcementRequests, setAnnouncementRequests] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:8081/announcements/list").then((response) => {
-      const announcementRequests = response.data;
-      setAnnouncements(
-        announcementRequests.map(({ title, deadline, content }) => ({
+      const data = response.data;
+      setAnnouncementRequests(
+        data.map(({ title, deadline, document: { documentId, content } }) => ({
+          id: documentId,
           title,
           deadline,
           content,
@@ -38,34 +29,21 @@ function AdminAnnouncementRequests() {
         <div className="header d-flex align-items-center">
           <Header username={"System Admin"} />
         </div>
-                <div className="announcements align-items-center">
-                    <h1>Announcement Requests</h1>
-                    <div className="row">
-                        {announcements.map((announcement) => (
-                            <div key={announcement.id} className="col-sm-6 col-md-4">
-                                <div className="card">
-                                    <div className="card-body">
-                                        <div className="announcement-details d-flex">
-                                            <h5 className="card-title">{announcement.title}</h5>
-                                        </div>
-                                        <div className="announcement-text">
-                                            <p className="card-text" onClick={handleContentClick} style={{cursor: 'pointer'}}>{announcement.content}</p>
-                                            <FontAwesomeIcon icon= {faCheck} color="green" size='2x' onClick={() => handleContentClick()} style={{cursor: 'pointer'}}/>
-                                            <FontAwesomeIcon icon= {faXmark} color="red" size='2x' onClick={() => handleContentClick()} style={{cursor: 'pointer'}}/>    
-                                        </div>
-                                        <div className="btn-group">
-                                                <button onClick={() => handleActionClick('Feedback', announcement.id)} className="btn btn-primary mr-1">Feedback</button>
-                                                <button onClick={() => handleActionClick('Ban', announcement.id)} className="btn btn-danger">Ban</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
+        <div className="announcements align-items-center">
+          <h1>Announcement Requests</h1>
+          <div className="announcement-requests-container">
+            {announcementRequests.map((announcementRequest, index) => (
+              <div key={index}>
+                <AnnouncementRequest
+                  announcementRequest={announcementRequest}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default AdminAnnouncementRequests;
---
+++ src/components/AnnouncementRequest/index.js
@@ -0,0 +1,102 @@
+import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import React from "react";
+import styles from "./announcement-request.module.css";
+
+function AnnouncementRequest({ announcementRequest }) {
+  const showAnnouncementRequest = () => {
+    const documentBase64 = announcementRequest.content;
+
+    if (!documentBase64) {
+      console.error("Document base64 data is missing");
+      return;
+    }
+
+    // Decode the Base64 string to binary
+    const binaryString = window.atob(documentBase64);
+    const len = binaryString.length;
+    console.log(binaryString);
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+
+    console.log(bytes);
+
+    // Create a Blob from the binary data
+    const pdfBlob = new Blob([bytes], { type: "application/pdf" });
+
+    // Generate a URL for the Blob
+    const pdfUrl = URL.createObjectURL(pdfBlob);
+
+    // Open the PDF in a new browser tab
+    window.open(pdfUrl, "_blank");
+  };
+
+  const approveAnnouncementRequest = () => {
+    axios
+      .put(
+        `http://localhost:8081/systemadmin/document/${announcementRequest.id}/approve`
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+  };
+  const disapproveAnnouncementRequest = () => {
+    axios
+      .put(
+        `http://localhost:8081/systemadmin/document/${announcementRequest.id}/disapprove`
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+  };
+  const banCompany = () => {};
+  const giveFeedback = () => {};
+
+  return (
+    <div className={styles.card} onClick={() => showAnnouncementRequest()}>
+      <div className={styles.left}>
+        <div>
+          <h2>{announcementRequest.title}</h2>
+          <p>{announcementRequest.deadline}</p>
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.feedbackBtn} onClick={giveFeedback}>
+            Feedback
+          </button>
+          <button className={styles.banBtn} onClick={banCompany}>
+            Ban
+          </button>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <FontAwesomeIcon
+          icon={faCheck}
+          color="green"
+          size="2x"
+          style={{ cursor: "pointer" }}
+          onClick={(event) => {
+            event.stopPropagation(); // Stop event propagation
+            approveAnnouncementRequest();
+          }}
+        />
+        <FontAwesomeIcon
+          icon={faXmark}
+          color="red"
+          size="2x"
+          style={{ cursor: "pointer" }}
+          onClick={(event) => {
+            event.stopPropagation(); // Stop event propagation
+            disapproveAnnouncementRequest();
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default AnnouncementRequest;
