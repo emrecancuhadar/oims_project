76c8c566f740c2c27925c6dc18a30d21fbbae6cd
caganozsir <caganozsir@gmail.com>
2024-05-17 20:48:16 +0300

    student homepage çözemedim my documents başlangıcı atıldı içeriği yok

--- src/Pages/StudentMyDocuments.js
+++ src/Pages/StudentMyDocuments.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import styles from "../CSS/StudentInternshipOpportunities.module.css";
+import Header from "../components/Header";
+import StudentSidebar from "../components/StudentSidebar";
+import { UserContext } from "../context/UserProvider";
+import InternshipOpportunity from "../components/InternshipOpportunity";
 
 function StudentMyDocuments() {
+  const { user } = useContext(UserContext);
+
   return (
-    <div>StudentMyDocuments</div>
+    <div className={styles.studentInternshipOpps}>
+      <StudentSidebar />
+      <div className={styles.mainContent}>
+          <Header username={user.name} />
+        <div className={styles.internships}>
+          <div className={styles.titleContainer}>
+            <h1>My Documents</h1>
+          </div>
+        </div>
+    </div>
+  </div>
   )
 }
 
