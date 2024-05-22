fd1ffc7c2443f97f3ec350f01a63e36869a03757
emreerol0 <emreerol.erol0@gmail.com>
2024-05-22 04:51:27 +0300

    depsec kontrol

--- src/Pages/DepSecEligibleStudents.js
+++ src/Pages/DepSecEligibleStudents.js
@@ -1,71 +1,87 @@
+import Modal from "@mui/material/Modal";
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import styles from "../CSS/DepSecEligibleStudents.module.css";
 import DepSecSidebar from "../components/DepSecSidebar";
 import Header from "../components/Header";
+import Popup from "../components/Popup";
 import { UserContext } from "../context/UserProvider";
+import EligibleStudentCard from "../components/EligibleStudentCard";
 
 
 function DepSecEligibleStudents() {
   const { user } = useContext(UserContext);
   const [students, setStudents] = useState([]);
+  const [popupOpen, setPopupOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [errorPopupOpen, setErrorPopupOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [email, setEmail] = useState("");
   
+
   useEffect(() => {
     axios
-    .get(`${process.env.REACT_APP_API_URL}/depsec/eligible-students/${user.id}`)
-    .then((response) => {
+      .get(`${process.env.REACT_APP_API_URL}/students/eligible-students`)
+      .then((response) => {
         const data = response.data;
         console.log("eligible-students", data);
         setStudents(
-          data.map(({ id, fullName, email, contactNumber }) => ({
-            id,
-            fullName,
-            email,
-            contactNumber,
-          }))
+          data.map(
+            ({
+              id,
+              fullName,
+              email , 
+              contactNumber,
+            }) => ({
+              id,
+              owner: fullName,
+              email,
+              contactNumber,
+            })
+          )
         );
-      })
-      .catch((error) => {
-        console.error("Error fetching interns:", error);
       });
-  }, [user.id]);
+  }, []);
 
-const StudentEntries = ({ student }) => (
-  <div className={styles.card}>
-    <div>
-      <h2>{student.name}</h2>
-      <div className={styles.numberAndMail}>
-        <p>
-          <span>Phone Number:</span> {student.phoneNumber}
-        </p>
-        <p>
-          <span>Mail:</span> {student.mail}
-        </p>
+// const StudentEntries = ({ student }) => (
+//   <div className={styles.card}>
+//     <div>
+//       <h2>{student.name}</h2>
+//       <div className={styles.numberAndMail}>
+//         <p>
+//           <span>Phone Number:</span> {student.phoneNumber}
+//         </p>
+//         <p>
+//           <span>Mail:</span> {student.mail}
+//         </p>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+return (
+  <div className={styles.DepSecEligibleStudents}>
+    <DepSecSidebar />
+    <div className={styles.DepSecEligibleStudentsMainContent}>
+      <Header username={user.name} />
+      <div className={styles.homepage}>
+        <div className={styles.titleContainer}>
+          <h1>Eligible Students</h1>
+        </div>
+        <div className={styles.downloadPdfContainer}>
+          <button className={styles.downloadPdfBtn}>
+            Download Pdf
+          </button>
+        </div>
+        <div className={styles.homepageContainer}>
+          {students.map((student, index) => (
+            <EligibleStudentCard key={index} student={student} />
+          ))}
+        </div>
       </div>
     </div>
   </div>
 );
-
-  return (
-    <div className={styles.DepSecEligibleStudents}>
-      <DepSecSidebar />
-      <div className={styles.DepSecEligibleStudentsMainContent}>
-        <Header username={user.name} />
-        <div className={styles.homepage}>
-          <div className={styles.titleContainer}>
-            <h1>Eligible Students</h1>
-          </div>
-          <div className={styles.homepageContainer}>
-            <div className={styles.cardContainer}>
-              {students.map((student) => (
-                <StudentEntries key={student.id} student={student} />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
 }
 
 export default DepSecEligibleStudents;
